04005a8c44a1fe0ee48f628aa09aee428192dcc7
Cheryl-Lao <cheryl.lao@mail.utoronto.ca>
2016-06-10 13:31:46 -0400

    More updates

--- island-game-master/Assets/Scripts/AddCarnivore.cs
+++ island-game-master/Assets/Scripts/AddCarnivore.cs
@@ -4,10 +4,21 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class AddCarnivore : MonoBehaviour {
+    public GameObject spriteToDuplicate;
+    public GameObject island_picture;
 
     void Add_Carn() {
 		if ((IslandGenerator.main_level.reset_state.carn_capacity >= (IslandGenerator.main_level.reset_state.num_carnivores + 1))) {
 			IslandGenerator.main_level.reset_state.num_carnivores += 1;
 		}
+
+
+        //Adding the sprite to the screen
+        float x = island_picture.GetComponent<Renderer>().bounds.size.x;
+        float y = island_picture.GetComponent<Renderer>().bounds.size.y;
+
+        Vector3 currentPosition = spriteToDuplicate.transform.position;
+        currentPosition = new Vector3(Random.Range((-x/2)+1, (x/2)-1), Random.Range((-y/2) + 10, (y/2) - 10), 0f);
+        GameObject tmpObj = GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity) as GameObject;
     }
 }
--- island-game-master/Assets/Scripts/RemoveCarnivore.cs
+++ island-game-master/Assets/Scripts/RemoveCarnivore.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class RemoveCarnivore : MonoBehaviour {
 	public Text levelInfo;
     public GameObject carnSprite; 
+
     void Remove_Carn() {
 		levelInfo.text = "";
         if (0 <= (IslandGenerator.main_level.reset_state.num_carnivores - 1)) {
             IslandGenerator.main_level.reset_state.num_carnivores -= 1;
         }
 
-        Destroy(carnSprite);
+        carnSprite.SetActive(false);
     }
 }
